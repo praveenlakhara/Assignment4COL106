f20eaa901c381ba5a07a0c33e8d10e7b888b6ed1
praveenlakhara <120555185+praveenlakhara@users.noreply.github.com>
2023-10-01 01:18:17 +0530

    Update entry.h

--- entry.h
+++ entry.h
@@ -1,18 +1,1 @@
-#include "string"
-#include "ulimitedrational.h"
-using namespace std;
 
-class SymEntry {
-public:
-    string key;
-    UnlimitedRational* val;
-    
-    SymEntry* left;
-    SymEntry* right;
-
-    SymEntry();
-    SymEntry(string k, UnlimitedRational* v);
-
-    ~SymEntry();
-    
-};
